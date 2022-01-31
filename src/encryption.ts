6b7724884e5c93790d2f0043ab78ad132a522bf7
a <a@example.com>
2022-02-01 08:57:25 +0900

    remove hex-to-array-buffer dependency

--- src/encryption.ts
+++ src/encryption.ts
@@ -1,13 +1,24 @@
 
 import { secretbox } from "tweetnacl";
 import { encodeUTF8 } from "tweetnacl-util";
-import hexToArrayBuffer from 'hex-to-array-buffer'
+
+const hexToUint8Array = (x: string) => {
+    x = x.toLowerCase()
+    if (x.slice(0, 2) == '0x') {
+        x = x.slice(2)
+    }
+    const y = []
+    for (let i = 0; i < x.length / 2; i++) {
+        y[i] = parseInt(x.slice(2 * i, 2 * (i + 1)), 16)
+    }
+    return new Uint8Array(y);
+}
 
 export const decryptPrediction = (encryptedContent: string, contentKey: string) => {
-    if (!contentKey) return void 0
-    
-    const contentKeyArray = new Uint8Array(hexToArrayBuffer(contentKey.slice(2)));
-    const encryptedContentArray = new Uint8Array(hexToArrayBuffer(encryptedContent.slice(2)));
+    if (!contentKey) return void 0;
+
+    const contentKeyArray = hexToUint8Array(contentKey);
+    const encryptedContentArray = hexToUint8Array(encryptedContent);
     const nonce = encryptedContentArray.slice(0, secretbox.nonceLength);
     const message = encryptedContentArray.slice(secretbox.nonceLength);
     const decrypted = secretbox.open(message, nonce, contentKeyArray);
